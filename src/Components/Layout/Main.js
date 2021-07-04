ab396fe4bed2ed2243e37b2397c834eb6f7b1d75
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-04 17:31:42 +0530

    main page rough layout

--- src/Components/Layout/Main.js
+++ src/Components/Layout/Main.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        height: '100%'
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        height: '100%',
+        borderRadius: '20px',
+        background: 'rgba(255,255,255,0.5)',
+        backdropFilter: 'blur(20px)'
+    },
+    halfpaper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        height: '48%',
+        borderRadius: '20px',
+        background: 'rgba(255,255,255,0.5)',
+        backdropFilter: 'blur(20px)'
+    },
+}));
 
 const Main = () => {
+    const classes = useStyles();
 
-    return(
-        <div>Hi there</div>
+    return (
+        <div className="main">
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={3}>
+                        <Paper className={classes.paper}>Homies</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Paper className={classes.paper}>Convos</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <div className="halfgrid">
+                            <Paper className={classes.halfpaper}>Mesies</Paper>
+                            <Paper className={classes.halfpaper}>Search</Paper>
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
     )
 }
 
