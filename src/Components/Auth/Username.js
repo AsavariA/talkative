55fe2cc0c2cdbd2c9a336a0dff591f24f1ab9e4f
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-04 15:37:33 +0530

    unique username functionality

--- src/Components/Auth/Username.js
+++ src/Components/Auth/Username.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Input from './Input';
 import useStyles from './styles';
 import { Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import fire from '../../services/fire';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const Username = ({userData, setUserData, handleNext}) => {
     const classes = useStyles();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        handleNext();
+        fire.firestore().collection("Users").doc('ALL_USERNAMES').get().then((doc) => {
+            var usernameList = doc.data().usernames;
+            var check = usernameList.find(element => element === userData.username)
+            check ? toast.error('Sorry, username is taken!') : handleNext();
+        });
     }
 
     const handleChange = (e) => {
@@ -17,6 +24,7 @@ const Username = ({userData, setUserData, handleNext}) => {
 
     return (
         <div>
+            <ToastContainer />
             <Container component="main" maxWidth="xs" className={classes.main}>
                 <Paper className={classes.paper} elevation={3}>
                     <Typography component="h1" variant="h5">Set Username</Typography>
