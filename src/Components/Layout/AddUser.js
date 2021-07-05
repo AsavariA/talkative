ac02a2209d434576d46e74b0d3c310cf4c37c32f
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-05 15:36:22 +0530

    search users

--- src/Components/Layout/AddUser.js
+++ src/Components/Layout/AddUser.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import { Paper, IconButton } from '@material-ui/core';
+import { Paper, IconButton, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        backgroundColor: 'rgba(247,212,226,0.5)',
-        padding: theme.spacing(0.5),
+        backgroundColor: 'rgba(247,212,226)',
+        paddingTop: theme.spacing(0.5),
+        paddingBottom: theme.spacing(0.5),
+        paddingLeft: theme.spacing(1),
     },
 }))
 
 const AddUser = ({ doc }) => {
     const classes = useStyles();
+    const email = JSON.parse(localStorage.getItem("user")).email;
 
     return (
         <>
             {
-                doc.username === ''
+                doc.username === '' || doc.email === email
                     ? null
                     : <div style={{ margin: '1rem 0' }}>
                         <Paper className={classes.paper}>
-                            <div style={{ display: 'flex', alignItems:'center', 'justifyContent': 'space-between' }}>
-                                {doc.username}
+                            <div style={{ display: 'flex', alignItems: 'center', 'justifyContent': 'space-between' }}>
+                                <div style={{ display: 'flex', alignItems: 'center'}}>
+                                    <Avatar alt="profile-photo" src={doc.photo} />
+                                    <p style={{ margin: '0 0.5rem'}}>{doc.username}</p>
+                                </div>
                                 <IconButton>
                                     <PersonAddIcon />
                                 </IconButton>
                             </div>
                         </Paper>
-
                     </div>
             }
         </>
--- src/Components/Layout/Search.js
+++ src/Components/Layout/Search.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import fire from '../../services/fire';
 import AddUser from './AddUser'
+import { TextField, InputAdornment } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Search = () => {
     const [userList, setUserList] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     fire.firestore()
         .collection('Users')
@@ -15,10 +18,39 @@ const Search = () => {
 
     return (
         <>
-            <div>Search</div>
+            <p style={{ marginBottom: '0.5rem'}}>PEOPLE YOU MIGHT KNOW</p>
+            <TextField
+                onChange={(e) => { setSearchTerm(e.target.value) }}
+                variant="outlined"
+                fullWidth
+                autoComplete="off"
+                size="small"
+                label="Search..."
+                type="text"
+                InputProps={{
+                endAdornment:
+                    <InputAdornment position="end">
+                        <SearchIcon />
+                    </InputAdornment>
+                }}
+            />
             {!userList
                 ? <p>Loading...</p>
-                : userList.map((doc) => <div key={doc.username}><AddUser doc={doc}/></div>)
+                // eslint-disable-next-line
+                : userList.filter((doc) => {
+                    if (searchTerm === '') {
+                        return doc
+                    } else if (doc.username.toLowerCase().includes(searchTerm.toLowerCase())) { 
+                        return doc
+                    }
+                }).map((doc, key) => {
+                    return (
+                        <div key={key}>
+                            <AddUser doc={doc} />
+                        </div>
+                    )
+                }
+                )
             }
         </>
     )
