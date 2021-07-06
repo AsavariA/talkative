49f25f3557c88d94a47ced98d62c572896ed1b72
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-06 20:41:38 +0530

    textfield and other changes

--- src/Components/Layout/Chat.js
+++ src/Components/Layout/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, InputAdornment, IconButton } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
@@ -10,6 +10,11 @@ const Chat = ({ activeChat, timeConverter }) => {
         justifyContent: 'space-between'
     }
     const email = JSON.parse(localStorage.getItem("user")).email;
+    const [msg, setMsg] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('submitted');
+    }
 
     return (
         <div style={styles}>
@@ -18,30 +23,35 @@ const Chat = ({ activeChat, timeConverter }) => {
                 {activeChat.chats.length > 0 ?
                     activeChat.chats.map((chat) => {
                         return (
-                            <div key={chat.time} className={chat.sender === email ? 'rightbubble' : 'leftbubble'}>
-                                <h3>{chat.msg}</h3>
-                                <p>{timeConverter(chat.time)}</p>
+                            <div key={chat.time} style={{display: 'flex', padding: '0.5rem 0', flexDirection: 'column', alignItems: chat.sender === email ? 'flex-end' : 'flex-start'}}>
+                                <div className={chat.sender === email ? 'rightbubble' : 'leftbubble'}>
+                                    <h3>{chat.msg}</h3>
+                                    <p>{timeConverter(chat.time)}</p>
+                                </div>
                             </div>
                         )
                     }) : null
                 }
             </div>
-            <TextField
-                variant="filled"
-                fullWidth
-                autoComplete="off"
-                size="small"
-                label="Type a message"
-                type="text"
-                InputProps={{
-                    endAdornment:
-                        <InputAdornment position="end">
-                            <IconButton>
-                                <SendIcon />
-                            </IconButton>
-                        </InputAdornment>
-                }}
-            />
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    variant="filled"
+                    fullWidth
+                    autoComplete="off"
+                    size="small"
+                    label="Type a message"
+                    type="text"
+                    onChange={(e) => setMsg(e.target.value)}
+                    InputProps={{
+                        endAdornment:
+                            <InputAdornment position="end">
+                                <IconButton type="submit">
+                                    <SendIcon />
+                                </IconButton>
+                            </InputAdornment>
+                    }}
+                />
+            </form>
         </div>
     )
 }
--- src/Components/Layout/FriendTile.js
+++ src/Components/Layout/FriendTile.js
@@ -25,11 +25,13 @@ const FriendTile = ({friend, setActiveChat, timeConverter}) => {
     return (
         <div className="list-tile" onClick={() => setActiveChat({email: friend.id, username: friend.data.username, chats: friend.data.chats})}>
             <Avatar alt="profile-photo" src={photo} />
-            <div>
+            <div style={{width: '60%'}}>
                 <h2>{friend.data.username}</h2>
-                <h3>{lastMessage.msg.length > 30 ? `${lastMessage.msg.slice(0,30)}. . .` : lastMessage.msg}</h3>
+                <h3>{lastMessage.msg.length > 20 ? `${lastMessage.msg.slice(0,20)}. . .` : lastMessage.msg}</h3>
             </div>
+            <div style={{width: '10%'}}>
                 <h3>{timeConverter(lastMessage.time)}</h3>
+            </div>    
         </div>
     )
 }
